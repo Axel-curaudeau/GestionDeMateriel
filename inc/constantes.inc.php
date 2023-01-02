e8766e6df5b99eab0752637f59e25a045924a559
ThomasRaymond <46885409+ThomasRaymond@users.noreply.github.com>
2023-01-02 23:52:59 +0100

    Update constantes.inc.php

--- inc/constantes.inc.php
+++ inc/constantes.inc.php
@@ -3,5 +3,4 @@
 !defined("BDD_HOST") && define("BDD_HOST","mysql:host=localhost:8889;dbname=qualilogproject;charset=utf8");
 !defined("BDD_USER") && define("BDD_USER","root");
 !defined("BDD_PSWD") && define("BDD_PSWD","root");
-
 ?>
