488199678c83885834799474aaf82299dd03a25e
ThomasRaymond <46885409+ThomasRaymond@users.noreply.github.com>
2023-01-02 23:52:45 +0100

    Add files via upload

---
+++ inc/constantes.inc.php
@@ -0,0 +1,7 @@
+<?php
+!defined("DOMAIN_URL") && define("DOMAIN_URL","http://localhost:8888");
+!defined("BDD_HOST") && define("BDD_HOST","mysql:host=localhost:8889;dbname=qualilogproject;charset=utf8");
+!defined("BDD_USER") && define("BDD_USER","root");
+!defined("BDD_PSWD") && define("BDD_PSWD","root");
+
+?>
